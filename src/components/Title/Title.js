0eea1803e18e250f3de2f4d2048b8ecfabcc7a90
Scott Sanford <scottsanford1217@gmail.com>
2020-05-23 13:01:18 -0500

    Initial Commit

---
+++ src/components/Title/Title.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import styles from './Title.module.css'
+
+const Title = () => {
+	return (
+		<div>
+			<span className={styles.main}>Main</span>
+			<span className={styles.subhead}>news</span>
+		</div>
+	)
+}
+
+export default Title
