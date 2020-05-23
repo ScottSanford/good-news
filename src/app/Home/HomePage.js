3c218fa68667d7f15bb7e345a004e7e33e3c2a97
Scott Sanford <scottsanford1217@gmail.com>
2020-05-23 15:31:59 -0500

    add title component and change logo height

--- src/app/Home/HomePage.js
+++ src/app/Home/HomePage.js
@@ -2,11 +2,13 @@ import React from 'react'
 import styles from './HomePage.module.css'
 import data from './mock'
 
+import Title from '../../components/Title/Title'
 
 const HomePage = () => {
 	console.log(data)
 	return (
 		<div>
+			<Title />
 			{data.map(anArticle => {
 				return <h1 key={anArticle.created_date}>{anArticle.title}</h1>
 			})}
