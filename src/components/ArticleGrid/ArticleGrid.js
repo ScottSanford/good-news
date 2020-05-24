b33859e8bf81e7ad64e01c3f5e93f392b6a471e8
Scott Sanford <scottsanford1217@gmail.com>
2020-05-23 21:09:56 -0500

    add hover effect animation

--- src/components/ArticleGrid/ArticleGrid.js
+++ src/components/ArticleGrid/ArticleGrid.js
@@ -8,8 +8,7 @@ const ArticleGrid = ({ articles }) => {
 	return (
 		<div className={styles.parent}>
 			{articles.map((anArticle, index) => {
-				const gridItemClass = index < 5 ? styles[`div${index + 1}`] : styles.divImplicit
-				console.log(index, gridItemClass, styles.divImplicit)
+				const gridItemClass = index < 5 ? styles[`article${index + 1}`] : styles.articleImplicit
 				return <ArticleGridItem
 					key={anArticle.created_date}
 					article={anArticle}
--- src/components/ArticleGrid/ArticleGridItem/ArticleGridItem.js
+++ src/components/ArticleGrid/ArticleGridItem/ArticleGridItem.js
@@ -1,23 +1,13 @@
 import React from 'react'
 import styles from './ArticleGridItem.module.css'
 
-function articleImage(img) {
-
-	const colorOverlay = 'rgba(153, 175, 235, .45)'
-
-	return {
-		backgroundImage: `linear-gradient(${colorOverlay}, ${colorOverlay}), url(${img})`,
-		backgroundSize: 'cover',
-		backgroundPosition: 'center',
-		minWidth: '100%',
-		minHeight: '100%',
-	}
-}
-
 const ArticleGridItem = ({ article, gridClass }) => {
+
 	const { multimedia } = article
+
 	return (
-		<div className={gridClass} style={articleImage(multimedia[0].url)}>
+		<div className={gridClass}>
+			<img src={multimedia[0].url} alt={multimedia[0].format} className={styles.articleImage} />
 			<div className={styles.articleInfo}>
 				<div className={styles.articleTitle}>{article.title}</div>
 				<div className={styles.articleDate}>{article.created_date}</div>
