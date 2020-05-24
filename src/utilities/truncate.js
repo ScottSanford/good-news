626e643240154cbde29e8a389cfb80fd0d4525c0
Scott Sanford <scottsanford1217@gmail.com>
2020-05-24 18:43:17 -0500

    refactor truncate function

--- src/utilities/truncate.js
+++ src/utilities/truncate.js
@@ -1,11 +1,10 @@
 const truncate = (str, num) => {
 	// If the length of str is less than or equal to num
 	// just return str--don't truncate it.
-	if (str.length <= num) {
-		return str
-	}
-	// Return str truncated with '...' concatenated to the end of str.
-	return str.slice(0, num) + '...'
+	return str.length <= num
+		? str
+		// Return str truncated with '...' concatenated to the end of str.
+		: `${str.slice(0, num)}...`
 }
 
 export default truncate
