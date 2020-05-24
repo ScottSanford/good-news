79ff9f4f2f68fade6549641bddcea957b84f51f8
Scott Sanford <scottsanford1217@gmail.com>
2020-05-23 22:20:21 -0500

    change media query & add moment date

--- src/components/ArticleGrid/ArticleGridItem/ArticleGridItem.js
+++ src/components/ArticleGrid/ArticleGridItem/ArticleGridItem.js
@@ -1,16 +1,18 @@
 import React from 'react'
 import styles from './ArticleGridItem.module.css'
+import moment from 'moment'
 
 const ArticleGridItem = ({ article, gridClass }) => {
 
 	const { multimedia } = article
+	const fromNowDate = moment(article.created_date).fromNow()
 
 	return (
 		<div className={gridClass}>
 			<img src={multimedia[0].url} alt={multimedia[0].format} className={styles.articleImage} />
 			<div className={styles.articleInfo}>
 				<div className={styles.articleTitle}>{article.title}</div>
-				<div className={styles.articleDate}>{article.created_date}</div>
+				<div className={styles.articleDate}>{fromNowDate}</div>
 			</div>
 		</div>
 	)
