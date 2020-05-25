24def739e2461a749dd7246bfab5eca274d83106
Scott Sanford <scottsanford1217@gmail.com>
2020-05-25 18:09:34 -0500

    add styles to paywall

--- src/components/Paywall/Paywall.js
+++ src/components/Paywall/Paywall.js
@@ -8,7 +8,7 @@ const Paywall = ({ menu }) => {
 		<div className={styles.Paywall}>
 			<div className={styles.PaywallTitle}>Looks like you've enjoyed this story!</div>
 			<div className={styles.PaywallText}>
-				We are unwavering in our commitment to bring the best storytelling in sports to our readers.
+				We all hate paywalls. We also hate bad design. Subscribe to get our latest tips and tricks.
 			</div>
 			<button className={styles.PaywallButton}>
 				<span className={styles.visible}>Subscribe</span>
