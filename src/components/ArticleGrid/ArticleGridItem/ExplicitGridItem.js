01f054a302b64dc0ef4c99fbd8e7f204b3852647
Scott Sanford <scottsanford1217@gmail.com>
2020-05-25 12:15:47 -0500

    add comments

--- src/components/ArticleGrid/ArticleGridItem/ExplicitGridItem.js
+++ src/components/ArticleGrid/ArticleGridItem/ExplicitGridItem.js
@@ -13,6 +13,15 @@ const ExplicitGridItem = ({ article, gridClass }) => {
 	const fromNowDate = moment(article.created_date).fromNow()
 
 	const handleArticleClick = () => {
+		/**
+		 * There are a few ways to pass data to child component.
+		 * A few reasons with this approach:
+		 * 1. I love the declarative style with React Router Hooks.
+		 * 2. NWT API returns the full schema back already (no need to made another api call).
+		 * 3. Child component should not be managing state (Dumb/Stateless component).
+		 * 4. If another API was needed, I would create a State/Smart Component,
+		 * perform API call/manage state, and create a Stateless/Dumb/UI component.
+		 */
 		history.push({
 			pathname: '/article',
 			state: { article }
