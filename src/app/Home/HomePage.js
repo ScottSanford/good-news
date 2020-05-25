d256a45580c1f17d0bd3d6f8592bea7a0cfdee08
Scott Sanford <scottsanford1217@gmail.com>
2020-05-25 00:28:15 -0500

    add an error state

--- src/app/Home/HomePage.js
+++ src/app/Home/HomePage.js
@@ -2,18 +2,24 @@ import React from 'react'
 import useFetch from '../../utilities/useFetch'
 
 import ArticleGrid from '../../components/ArticleGrid/ArticleGrid'
+import Error from '../../components/Error/Error'
 import GridSkeleton from '../../components/GridSkeleton/GridSkeleton'
 import Title from '../../components/Title/Title'
 
 const HomePage = () => {
 
-	const apiUrl = `https://api.nytimes.com/svc/topstories/v2/sports.json?api-key=${process.env.REACT_APP_NYTAPIKEY}`
-	const pageTitle = 'Explore'
-	const { response } = useFetch(apiUrl)
+	// api key accessed through env variable
+	const apiKey = process.env.REACT_APP_NYTAPIKEY
+	const apiUrl = `https://api.nytimes.com/svc/topstorie/v2/sports.json?api-key=${apiKey}`
+	const { response, error } = useFetch(apiUrl)
 
-	const activeComponent = !response
-		? <GridSkeleton />
-		: <ArticleGrid articles={response.results} />
+	const pageTitle = 'Explore'
+
+	const activeComponent = error
+		? <Error />
+		: !response
+			? <GridSkeleton />
+			: <ArticleGrid articles={response.results} />
 
 	return (
 		<div>
---
+++ src/components/Error/Error.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import styles from './Error.module.css'
+import errorImage from '../../error500.png'
+
+
+const Error = ({ menu }) => {
+
+	return (
+		<div className={styles.Error}>
+			<h3 className={styles.errorTitle}>Uh Oh!</h3>
+			<div className={styles.errorSubtext}>Something went wrong on our end.</div>
+			<div className={styles.errorSubtext}>Don't worry, it's not you - it's us. Sorry about that.</div>
+			<img className={styles.errorImage} src={errorImage} alt="network error" />
+		</div>
+	)
+}
+
+export default Error
