998f802f560e7303192eac9bbd7318e9d9273507
Scott Sanford <scottsanford1217@gmail.com>
2020-05-28 16:40:46 -0500

    rename files

--- src/components/ArticleGrid/ArticleGrid.js
+++ src/components/ArticleGrid/ArticleGrid.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import styles from './ArticleGrid.module.css'
 
-import ExplicitGridItem from './ArticleGridItem/ExplicitGridItem'
-import ImplicitGridItem from './ArticleGridItem/ImplicitGridItem'
-import WrappedArticleGridItem from './ArticleGridItem/ArticleGridItem'
+import ExplicitGridItem from './WrappedGridArticle/ExplicitGridItem'
+import ImplicitGridItem from './WrappedGridArticle/ImplicitGridItem'
+import WrappedGridArticle from './WrappedGridArticle/WrappedGridArticle'
 
 
 const ArticleGrid = ({ articles }) => {
@@ -20,8 +20,8 @@ const ArticleGrid = ({ articles }) => {
 				}
 
 				return condition
-					? WrappedArticleGridItem(ExplicitGridItem, passThroughProps)
-					: WrappedArticleGridItem(ImplicitGridItem, passThroughProps)
+					? WrappedGridArticle(ExplicitGridItem, passThroughProps)
+					: WrappedGridArticle(ImplicitGridItem, passThroughProps)
 			})}
 		</div>
 	)
--- src/components/ArticleGrid/WrappedGridArticle/WrappedGridArticle.js
+++ src/components/ArticleGrid/WrappedGridArticle/WrappedGridArticle.js
@@ -1,9 +1,9 @@
 import React from 'react'
 
 // Higher Order Component that returns either an ExplicitGridItem or ImplicitGridItem
-const WrappedArticleGridItem = (WrappedComponent, passThroughProps) => {
+const WrappedGridArticle = (WrappedComponent, passThroughProps) => {
 
 	return <WrappedComponent {...passThroughProps} />
 }
 
-export default WrappedArticleGridItem
+export default WrappedGridArticle
