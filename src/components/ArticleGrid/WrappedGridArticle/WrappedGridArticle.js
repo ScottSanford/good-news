dedca0218ac9ac0c51fe4608e53f54dad3fea189
Scott Sanford <scottsanford1217@gmail.com>
2020-05-28 16:50:23 -0500

    add better comment

--- src/components/ArticleGrid/WrappedGridArticle/WrappedGridArticle.js
+++ src/components/ArticleGrid/WrappedGridArticle/WrappedGridArticle.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-// Higher Order Component that returns either an ExplicitGridItem or ImplicitGridItem
+// Higher Order Component that returns either a Small or Large Article Card.
 const WrappedGridArticle = (WrappedComponent, passThroughProps) => {
 
 	return <WrappedComponent {...passThroughProps} />
